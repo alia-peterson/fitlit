9cbb5802b6a7927cbd0cdf1b68d1ee4ac223e332
Ben Prat <bvprat@gmail.com>
2020-12-09 20:10:00 -0700

    Refactor filter method usage.

--- src/userRepository.js
+++ src/userRepository.js
@@ -1,31 +1,29 @@
 class UserRepository {
-  constructor( data = [] ) {
-  	this.users = data
+  constructor(data = []) {
+    this.users = data
     this.currentUser = ''
   }
 
-  returnUserData( userID ) {
-  	const userData = this.users.find( user => user.id === userID )
-  	return userData
+  returnUserData(userID) {
+    const userData = this.users.find(user => user.id === userID)
+    return userData
   }
 
   calculateAverageStepGoal() {
-  	return this.users.reduce( (steps, user) => {
+    return this.users.reduce((steps, user) => {
       return steps += user.dailyStepGoal / this.users.length
     }, 0)
   }
 
-  populateHydrationData( hydrationInput = hydrationData ) {
+  populateHydrationData(hydrationInput = hydrationData) {
     this.users.forEach(user => {
-      hydrationInput.filter(entry => {
-        if (entry.userID === user.id) {
-          user.hydrationEntry.push(entry)
-        }
+      user.hydrationEntry = hydrationInput.filter(entry => {
+        return entry.userID === user.id
       })
-
     })
-  }
+  })
 }
+
 
 
 
